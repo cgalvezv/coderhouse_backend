48780441c7f581819d6ac9131eb84120c54fc3a6
Camilo Gálvez Vidal <camilogalvezv@gmail.com>
2021-06-09 19:21:59 -0400

    Se implementa vistas pug al archivo server

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 //Desafío 8 - Express avanzado - parte 2
 //author: Camilo Gálvez Vidal
 const express = require('express');
+const fetch = require('node-fetch');
 
 const app = express();
+// pongo a escuchar el servidor en el puerto indicado
+const puerto = 8080;
+
+// Arreglo que guarda información en relación a los inputs del formulario para agregar productos
+const formIngresoProducto = [
+    {
+        title: 'Titulo',
+        name: 'title',
+        input_title: 'inputTitle'
+    },
+    {
+        title: 'Precio',
+        name: 'price',
+        input_title: 'inputPrice'
+    },
+    {
+        title: 'URL',
+        name: 'thumbnail',
+        input_title: 'inputThumbnail'
+    },
+]
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// indica el directorio donde se almacenaran las plantillas
+app.set('views', './src/views');
+// se indica el motor de plantillas a utilizar
+app.set('view engine', 'pug');
+
+
 
 
 //#region LOGICA MIDDLEWARE ERROR HANDLER
@@ -17,8 +46,18 @@ app.use((err, req, res, next) => {
 //#endregion
 
 //#region LOGICA STATICS FILE
+app.get('/productos/vista', async (req, res) => {
+    const responseApi = await fetch(`http://localhost:${puerto}/api/productos/listar`,{
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    })
+    const objectResponseApi = JSON.parse(await responseApi.text());
+    const data = objectResponseApi.error ? [] : objectResponseApi;
+    res.render('get_products.pug', { productos: data });
+});
+
 app.get('/', (req, res) => {
-    res.sendFile('index');
+    res.render('add_products.pug', { inputs: formIngresoProducto });
 });
 //#region 
 
@@ -27,9 +66,6 @@ const router = require('./src/routes/productos');
 app.use('/api/productos', router);
 //#endregion
 
-
-// pongo a escuchar el servidor en el puerto indicado
-const puerto = 8080;
 
 const server = app.listen(puerto, () => {
     console.log(`servidor escuchando en http://localhost:${puerto}`);
