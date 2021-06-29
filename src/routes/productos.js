7ac16b93aa2bfc4639e090f7117902831a219f17
Camilo Gálvez Vidal <camilogalvezv@gmail.com>
2021-06-29 17:23:51 -0400

    Se implementa rutas solicitadas para la API

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,40 +1,41 @@
 const express = require('express');
-const router = express.Router();
+const middleware = require('../middleware');
+const routerProductos = express.Router();
 
-const productos = require('../classes/productos');
-
+const productos = require('../api/productos');
+productos.init();
 //GET: Obtener todo el listado de productos
-router.get('/listar', (req, res) => {
+routerProductos.get('/listar', async (req, res) => {
     const response = productos.get();
-    if (response.error) res.status(response.code).json({ error: true, msg: response.msg });
+    if (response.error) res.status(response.code).json({ error: -1, msg: response.msg });
     res.status(200).json(response);
 })
 //GET: Obtener un producto en específico, recibiendo el ID desde el uri
-router.get('/listar/:id', (req, res) => {
+routerProductos.get('/listar/:id', (req, res) => {
     const { id } = req.params;
     const response = productos.get(id);
-    if (response.error) res.status(response.code).json({ error: true, msg: response.msg });
+    if (response.error) res.status(response.code).json({ error: -1, msg: response.msg });
     res.status(200).json(response);
 })
 //POST: Agregar un nuevo producto al listado de productos
-router.post('/guardar', (req, res) => {
-    const { title, price, thumbnail } = req.body;
-    const response = productos.add(title, price, thumbnail)
-    res.redirect('/');
-    // res.status(200).json(response);
-})
-//PUT: Agregar un nuevo producto al listado de productos
-router.put('/actualizar/:id', (req, res) => {
-    const { id } = req.params;
-    const { title, price, thumbnail } = req.body;
-    const response = productos.edit(id, title, price, thumbnail)
+routerProductos.post('/agregar', middleware.auth.isAdmin, (req, res) => {
+    const { titulo, descripcion, codigo, precio, foto_url, stock } = req.body;
+    const response = productos.add(titulo, descripcion, codigo, precio, foto_url, stock)
     res.status(200).json(response);
 })
-//DELETE: Agregar un nuevo producto al listado de productos
-router.delete('/borrar/:id', (req, res) => {
+//PUT: Actualizar un producto en el listado de productos
+routerProductos.put('/actualizar/:id', middleware.auth.isAdmin,(req, res) => {
     const { id } = req.params;
-    const response = productos.delete(id)
+    const { titulo, descripcion, codigo, precio, foto_url, stock } = req.body;
+    const response = productos.edit(id, titulo, descripcion, codigo, precio, foto_url, stock)
+    res.status(200).json(response);
+})
+//DELETE: Borrar un producto del listado
+routerProductos.delete('/borrar/:id', middleware.auth.isAdmin,(req, res) => {
+    const { id } = req.params;
+    const response = productos.delete(Number(id))
+    if (response.error) res.status(response.code).json({ error: -1, msg: response.msg });
     res.status(200).json(response);
 })
 
-module.exports = router;
+module.exports = routerProductos;
