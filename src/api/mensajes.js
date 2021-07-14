378b23aa1d470357565eabeef3de49e2594fd52d
Camilo Gálvez Vidal <camilogalvezv@gmail.com>
2021-07-14 09:35:54 -0400

    Se crea controlador base y se actualizan los controladores existentes para agregando herencia desde la base

--- src/api/mensajes.js
+++ src/api/mensajes.js
@@ -1,33 +1,12 @@
 const Mensaje = require('../models/mensaje');
+const BaseController = require('./base');
 
-class MensajesController {
-    constructor() {}
-
-    /**
-     * Método que agreaga un mensaje en el chat
-     * @param {object} mensaje es el mensaje que se desea agregar
-     * @returns un 1 si el mensaje fue agregado de forma exitosa
-     */
-    async add(mensaje) {
-        try {
-            return await Mensaje.guardar(mensaje); 
-        } catch (error) {
-            throw error;
-        }
-    }
-
-    /**
-     * Método que obtiene todos los mensajes del chat
-     * @returns el listado de mensajes del chat para
-     */
-    async getAll() {
-        try {
-            return await Mensaje.obtenerTodos();
-        } catch (error) {
-            throw error;
-        }
+class MensajeController extends BaseController {
+    constructor() {
+        super(Mensaje);
     }
 }
 
 // exporto una instancia de la clase
-module.exports = new MensajesController();
+module.exports = new MensajeController();
+
