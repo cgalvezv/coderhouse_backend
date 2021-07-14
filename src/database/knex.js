e67a4933746483b521cbce39568511bbae31ebc8
Camilo Gálvez Vidal <camilogalvezv@gmail.com>
2021-07-14 09:37:49 -0400

    Se eliminan archivos innecesarios para el desafío

--- src/database/knex.js
+++
@@ -1,9 +0,0 @@
-const options = require('./../config/database');
-const sqlite3 = require('knex')(options.sqlite3);
-const mysql = require('knex')(options.mysql);
-
-// exporto el objeto para usarlo en otros modulos
-module.exports = {
-    sqlite3,
-    mysql
-};
