54c70020c4b39ed46cb8eb9618f789e9240cdc20
Camilo Gálvez Vidal <camilogalvezv@gmail.com>
2021-07-14 09:37:10 -0400

    Se actualiza las rutas utilizando los controladores con moongose

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,39 +1,54 @@
 const express = require('express');
 const router = express.Router();
-
-const productosController = require('../api/productos');
+const controller = require('../api/productos');
 
 //GET: Obtener todo el listado de productos
 router.get('/listar', async (req, res) => {
-    const response = await productosController.get();
-    if (response.error) res.status(response.code).json({ error: true, msg: response.msg });
-    res.status(200).json(response);
+    try {
+        const response = await controller.findAll();
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 })
 //GET: Obtener un producto en específico, recibiendo el ID desde el uri
-router.get('/listar/:id', (req, res) => {
-    const { id } = req.params;
-    const response = productosController.get(id);
-    if (response.error) res.status(response.code).json({ error: true, msg: response.msg });
-    res.status(200).json(response);
+router.get('/listar/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await controller.findById(id);
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 })
 //POST: Agregar un nuevo producto al listado de productos
 router.post('/guardar', async (req, res) => {
-    const { title, price, thumbnail } = req.body;
-    const response = await productosController.add(title, price, thumbnail)
-    res.status(200).json(response);
+    try {
+        const response = await controller.create(req.body)
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 })
 //PUT: Agregar un nuevo producto al listado de productos
 router.put('/actualizar/:id', async (req, res) => {
-    const { id } = req.params;
-    const { title, price, thumbnail } = req.body;
-    const response = await productosController.edit(id, title, price, thumbnail)
-    res.status(200).json(response);
+    try {
+        const { id } = req.params;
+        const response = await controller.update(id, req.body)
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 })
 //DELETE: Agregar un nuevo producto al listado de productos
 router.delete('/borrar/:id', async (req, res) => {
-    const { id } = req.params;
-    const response = await productosController.delete(id)
-    res.status(200).json(response);
+    try {
+        const { id } = req.params;
+        const response = await controller.remove(id)
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
 })
 
 module.exports = router;
