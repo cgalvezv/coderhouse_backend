96a2a993265dd0ff30db3e2066c5d87c6206e92d
Camilo Gálvez Vidal <camilogalvezv@gmail.com>
2021-08-26 18:23:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-//Desafío 28 - Global process y Child process
+//Desafío 31 - Logger Parte 1
 //author: Camilo Gálvez Vidal
 const express = require('express');
+const compression = require('compression');
 const app = express();
+// Lógica de compresión GZip
+// app.use(compression());
 const fetch = require('node-fetch');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -9,16 +12,17 @@ const handlebars = require('express-handlebars');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const MongoStore = require('connect-mongo');
-require('dotenv').config();
-const controller = require('./src/api/mensajes');
-const config = require('./src/config/config.json');
 const http = require('http').Server(app);
 const { fork } = require('child_process');
 const io = require('socket.io')(http);
 const cluster = require('cluster')
+const controller = require('./src/api/mensajes');
+const log4js = require('log4js');
+require('dotenv').config();
+require('./src/database/connection');
+const config = require('./src/config/config.json');
 const numCPUs = require('os').cpus().length
 const CON_CHILD_PROCESS_FORK = !false
-require('./src/database/connection');
 
 
 let messages = [];
@@ -40,181 +44,6 @@ process.argv.forEach((arg, index) => {
 });
 
 //#endregion
-passport.use(new FacebookStrategy({
-  clientID: facebookClientID,
-  clientSecret: facebookClientSecret,
-  callbackURL: '/auth/facebook/callback',
-  profileFields: ['id', 'displayName', 'photos', 'emails'],
-  scope: ['email']
-}, function(accessToken, refreshToken, profile, done) {
-    let userProfile = profile;
-    return done(null, userProfile);
-}));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
-const advancedOptions = {
-    useNewUrlParser: true, useUnifiedTopology: true
-}
-
-// Configuraciones para Express
-app.use(express.static('./public'));
-app.engine(
-    "hbs",
-    handlebars({
-      extname: ".hbs",
-      defaultLayout: 'index.hbs',
-    })
-);
-app.set("view engine", "hbs");
-app.set("views", "./src/views");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
-app.use(session({
-    store: MongoStore.create({ 
-        mongoUrl: process.env.MONGO_URL,
-        mongoOptions: advancedOptions
-    }),
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false,
-    rolling: true,
-    cookie: {
-        maxAge: 600000 // 10 min de duración de la sesión
-    }
-}))
-app.use(passport.initialize());
-app.use(passport.session());
-
-//#region LOGICA ROUTER
-const routerProductos = require('./src/routes/productos');
-app.use('/api/productos', routerProductos);
-//#endregion
-
-
-//#region MANEJO LOGIN
-// Vistas y llamadas para página login con Facebook
-app.get('/login', (req,res) => {
-    if(req.isAuthenticated()){
-        res.render("home", {
-          nombre: req.user.displayName,
-          foto: req.user.photos[0].value,
-          email: req.user.emails[0].value      
-        })
-    }
-    else {
-        res.sendFile(process.cwd() + '/public/login.html')
-    }
-})
-
-app.get('/info', (req,res) => {
-    // Argumentos de entrada
-    const args = JSON.stringify(process.argv);
-    // Nombre de plataforma (Sistema operativo)
-    const so = process.platform;
-    // Versión de NodeJS
-    const vNode = process.version;
-    // Uso de memoria
-    const memory = JSON.stringify(process.memoryUsage(), null, 2);
-    // Path de ejecución
-    const pathExec = process.execPath;
-    // Process ID
-    const processID = process.pid;
-    // Carpeta corriente
-    const cwd = process.cwd();
-    res.render("info", { args, so, vNode, memory, pathExec, processID, cwd, numCPUs })
-})
-
-app.get('/auth/facebook', passport.authenticate('facebook'));
-app.get('/auth/facebook/callback', passport.authenticate('facebook',
-  { successRedirect: '/home', 
-    failureRedirect: '/faillogin' }
-));
-
-app.get('/home', (req,res) => {
-   res.redirect('/')        
-})
-
-if(CON_CHILD_PROCESS_FORK) {
-    let randoms = fork('./src/utils/random.js');
-
-    const taskId = 0;
-    const tasks = {};
-
-    function addTask(data, callback) {
-        const id = taskId++;
-        randoms.send({id: id, data: data});
-        tasks[id] = callback;
-    };
-
-    randoms.on('message', function(message) {
-        tasks[message.id](message);
-    });
-    
-    app.get('/randoms', async (req,res) => {
-        addTask(req.query.cant || 100000000, randoms => {
-            res.json(randoms)
-        });
-    })
-}
-else {
-    app.get('/randoms', async (req,res) => {
-        res.send('<h2 style="color: orangered;">randoms -> no implementado!</h2>')
-    })
-}
-
-app.get('/faillogin', (req,res) => {
-    res.render('login-error', {});
-})
-
-app.get('/logout', (req,res) => {
-    let nombre = req.user.displayName
-    req.logout()
-    res.render("logout", { nombre })
-})
-//#endregion
-
-
-//#region MANEJO SOCKETS
-io.on('connection', async (socket) => {
-    const responseProducts = await fetch(`http://localhost:${puerto}/api/productos/listar`, {
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        method: 'GET'
-    });
-    products = await responseProducts.json();
-    io.sockets.emit('productos', products)
-    socket.on('update', async (isOK) => {
-        if (isOK) {
-            const responseProducts = await fetch(`http://localhost:${puerto}/api/productos/listar`, {
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                method: 'GET'
-            });
-            products = await responseProducts.json();
-            io.sockets.emit('productos', products)
-        }
-    })
-
-    messages = await controller.getAll();
-    io.sockets.emit('messages', messages);
-    socket.on('new-message', async (msj) => {
-        await controller.create(msj)
-        messages = await controller.getAll();
-        io.sockets.emit('messages', messages);
-    })
-});
-//#endregion
-
 if (modoEjecucion === 'CLUSTER') {
     /* MASTER */
     if(cluster.isMaster) {
@@ -242,6 +71,207 @@ if (modoEjecucion === 'CLUSTER') {
         });
     }
 } else {
+    //#region MANEJO DE LOGIN
+    passport.use(new FacebookStrategy({
+        clientID: facebookClientID,
+        clientSecret: facebookClientSecret,
+        callbackURL: '/auth/facebook/callback',
+        profileFields: ['id', 'displayName', 'photos', 'emails'],
+        scope: ['email']
+    }, function(accessToken, refreshToken, profile, done) {
+        let userProfile = profile;
+        return done(null, userProfile);
+    }));
+    
+    passport.serializeUser(function(user, cb) {
+        cb(null, user);
+    });
+    
+    passport.deserializeUser(function(obj, cb) {
+        cb(null, obj);
+    });
+    
+    const advancedOptions = {
+        useNewUrlParser: true, useUnifiedTopology: true
+    }
+    //#endregion
+    
+    //#region MANEJO DE LOGGER
+    log4js.configure({
+        appenders: {
+            logConsole: { type: 'console' },
+            logFileWarn: { type: 'file', filename: 'warn.log' },
+            logFileError: { type: 'file', filename: 'error.log' }
+        },
+        categories: {
+            default: { appenders: ['logConsole'], level: 'trace' },
+            info: { appenders: ['logConsole'], level: 'info' },
+            warn: { appenders: ['logFileWarn'], level: 'warn' },
+            error: { appenders: ['logFileError'], level: 'error' }
+        }
+    });
+    const loggerInfo = log4js.getLogger('info');
+    const loggerWarn = log4js.getLogger('warn');
+    const loggerError = log4js.getLogger('error');
+    //#endregion
+    
+    // Configuraciones para Express
+    app.use(express.static('./public'));
+    app.engine(
+        "hbs",
+        handlebars({
+            extname: ".hbs",
+            defaultLayout: 'index.hbs',
+        })
+    );
+    app.set("view engine", "hbs");
+    app.set("views", "./src/views");
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser())
+    app.use(session({
+        store: MongoStore.create({ 
+            mongoUrl: process.env.MONGO_URL,
+            mongoOptions: advancedOptions
+        }),
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: false,
+        rolling: true,
+        cookie: {
+            maxAge: 600000 // 10 min de duración de la sesión
+        }
+    }))
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
+    //#region LOGICA ROUTER
+    const routerProductos = require('./src/routes/productos');
+    app.use('/api/productos', routerProductos);
+    //#endregion
+    
+    
+    //#region MANEJO LOGIN
+    // Vistas y llamadas para página login con Facebook
+    app.get('/login', (req,res) => {
+        if(req.isAuthenticated()){
+            loggerWarn.warn(`Sesión ya iniciada!!!`)
+            loggerInfo.info('Re-dirigiendo al home...')
+            res.render("home", {
+                nombre: req.user.displayName,
+                foto: req.user.photos[0].value,
+                email: req.user.emails[0].value      
+            })
+        }
+        else {
+            loggerInfo.info('Re-dirigiendo al inicio de sesión...')
+            res.sendFile(process.cwd() + '/public/login.html')
+        }
+    })
+    
+    app.get('/info', (req,res) => {
+        // Argumentos de entrada
+        const args = JSON.stringify(process.argv);
+        // Nombre de plataforma (Sistema operativo)
+        const so = process.platform;
+        // Versión de NodeJS
+        const vNode = process.version;
+        // Uso de memoria
+        const memory = JSON.stringify(process.memoryUsage(), null, 2);
+        // Path de ejecución
+        const pathExec = process.execPath;
+        // Process ID
+        const processID = process.pid;
+        // Carpeta corriente
+        const cwd = process.cwd();
+        res.render("info", { args, so, vNode, memory, pathExec, processID, cwd, numCPUs })
+    })
+    
+    app.get('/auth/facebook', passport.authenticate('facebook'));
+    app.get('/auth/facebook/callback', passport.authenticate('facebook',
+        { successRedirect: '/home', 
+        failureRedirect: '/faillogin' }
+    ));
+    
+    app.get('/home', (req,res) => {
+        res.redirect('/')        
+    })
+    
+    if(CON_CHILD_PROCESS_FORK) {
+        let calculo = fork('./src/utils/random.js');
+
+        var taskId = 0;
+        var tasks = {};
+
+        function addTask(data, callback) {
+            var id = taskId++;
+            calculo.send({id: id, data: data});
+            tasks[id] = callback;
+        };
+
+        calculo.on('message', function(message) {
+            tasks[message.id](message);
+        });
+        
+        app.get('/randoms', async (req,res) => {
+            addTask(req.query.cant || 100000000, randoms => {
+                res.json(randoms)
+            });
+        })
+    }
+    else {
+        app.get('/randoms', async (req,res) => {
+            loggerWarn.warn(`Randoms no implementado!!!`)
+            res.send('<h2 style="color: orangered;">randoms -> no implementado!</h2>')
+        })
+    }
+    
+    app.get('/faillogin', (req,res) => {
+        loggerError.error('Error de Login!!!')
+        res.render('login-error', {});
+    })
+    
+    app.get('/logout', (req,res) => {
+        let nombre = req.user.displayName
+        req.logout()
+        loggerInfo.info('Cerrando sesión...')
+        res.render("logout", { nombre })
+    })
+    //#endregion
+    
+    
+    //#region MANEJO SOCKETS
+    io.on('connection', async (socket) => {
+        const responseProducts = await fetch(`http://localhost:${puerto}/api/productos/listar`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'GET'
+        });
+        products = await responseProducts.json();
+        io.sockets.emit('productos', products)
+        socket.on('update', async (isOK) => {
+            if (isOK) {
+                const responseProducts = await fetch(`http://localhost:${puerto}/api/productos/listar`, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    method: 'GET'
+                });
+                products = await responseProducts.json();
+                io.sockets.emit('productos', products)
+            }
+        })
+    
+        messages = await controller.getAll();
+        io.sockets.emit('messages', messages);
+        socket.on('new-message', async (msj) => {
+            await controller.create(msj)
+            messages = await controller.getAll();
+            io.sockets.emit('messages', messages);
+        })
+    });
+    //#endregion
     // pongo a escuchar el servidor en el puerto indicado
     const server = http.listen(puerto, () => {
         console.log(`servidor escuchando en http://localhost:${puerto}`);
